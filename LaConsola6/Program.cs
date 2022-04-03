eebd2074886bc29cbaf4f8bf3d704ebc4111cdf1
JoelCAI <joelargentina12345@gmail.com>
2022-04-03 14:02:18 -0300

    Agregar archivos de proyecto.

---
+++ LaConsola6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LaConsola6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Sistema s = new Sistema();
+			s.Iniciar();
+		}
+
+	}
+}
